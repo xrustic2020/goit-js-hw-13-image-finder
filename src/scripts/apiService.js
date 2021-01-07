b755a5db726c639b1ddfaca4e6ab3b85f00328a6
Tisanyuk Ruslan <ruslantisanykot@gmail.com>
2021-01-07 19:56:35 +0200

    added base project config

---
+++ src/scripts/apiService.js
@@ -0,0 +1,7 @@
+const API_KEY = '19804146-226f6b2b23308d5d1fd703e5d';
+const BASE_URL =
+  `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=1&per_page=12&key=${API_KEY}`;
+
+export default {
+
+}
